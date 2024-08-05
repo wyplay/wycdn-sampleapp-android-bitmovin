2c15a0e45f192e5c27d1e8ea4c5992342d5d5102
Pierre MAZOYER <pmazoyer@wyplay.com>
2024-08-05 18:04:20 +0200

    Add HLS support to exoplayer

--- wycdn-sampleapp/build.gradle.kts
+++ wycdn-sampleapp/build.gradle.kts
@@ -78,8 +78,9 @@ dependencies {
 
     // Media3
     implementation(libs.androidx.media3.exoplayer)
-    implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.exoplayer.hls)
+    implementation(libs.androidx.media3.ui)
 
     // Testing
     testImplementation(libs.junit)
