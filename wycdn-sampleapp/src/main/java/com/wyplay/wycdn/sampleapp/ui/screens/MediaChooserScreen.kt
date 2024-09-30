171429a49ef392e17aa996cb83061defee6399b3
Pierre MAZOYER <pmazoyer@wyplay.com>
2024-09-30 15:13:45 +0200

    Replace exoplayer with bitmovin player

--- wycdn-sampleapp/src/main/java/com/wyplay/wycdn/sampleapp/ui/screens/MediaChooserScreen.kt
+++ wycdn-sampleapp/src/main/java/com/wyplay/wycdn/sampleapp/ui/screens/MediaChooserScreen.kt
@@ -9,6 +9,7 @@
 
 package com.wyplay.wycdn.sampleapp.ui.screens
 
+
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -37,7 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -136,16 +137,11 @@ private fun MediaList(
     LaunchedEffect(Unit) {
         focusedIndex = -1
     }
-    LazyColumn(
-        modifier
-            .fillMaxSize()
 
-        ) {
+    LazyColumn(modifier.fillMaxSize()) {
         itemsIndexed(mediaList) { index, mediaItem ->
-            Log.d("theindex",index.toString() +" focuesindex: "+focusedIndex.toString())
-
             val backgroundColor = if (index == focusedIndex) MaterialTheme.colorScheme.primary
-            else Transparent
+            else Color.Transparent
 
             val textColor = if (index == focusedIndex) MaterialTheme.colorScheme.onPrimary
             else MaterialTheme.colorScheme.onSurface
@@ -159,7 +155,8 @@ private fun MediaList(
                     }
                 }
                 .focusable()
-            ){
+            ) {
+
                 Text(
                     text = mediaItem.mediaMetadata.title.toString(),
                     modifier = Modifier
